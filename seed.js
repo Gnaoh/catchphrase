9e3a974c787123488e6086b89f6642a9d3635315
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-01 09:27:22 -0700

    added gitignore

--- seed.js
+++ seed.js
@@ -7,7 +7,7 @@ var phrase_list =[
   {word: "Object" , definition: "is something that exists"},
   {word: "Variable" , definition: "a symbol representing a quantity that assumes a range of values"},
   {word: "Function" , definition: "user-defined or built-in set of statements that perform a task"},
-  {word: "Nathan Allen" , definition: "Software Engineer / WDI21 Instructor from Ohio. "},
+  {word: "Nathan Allen" , definition: "Software Engineer / WDI21 Instructor from Ohio "},
   {word: "JavaScript" , definition: "a scripting language developed by Netscape for HTML documents"}, 
   {word: "Operator" , definition: "performs a function on one or more operands or variables"},
   {word: "Script" , definition: "one or more JavaScript commands"},
@@ -24,7 +24,7 @@ var phrase_list =[
   {word: "Boostrap" , definition: "a framework for developing responsive, mobile first projects on the web"},
   {word: "Array" , definition: "a global object that is used in the construction of high-level, list-like objects"},
   {word: "AJAX" , definition: "Asynchronous JavaScript And XML (AJAX) allows us to make requests to a server"},
-  {word: "application" , definition: "Application Program Interfaces (APIs)"},
+  {word: "API" , definition: "Application Program Interfaces"},
   {word: "Anchors" , definition: "a piece of text that can be the target of a hypertext link"},  
   {word: "Justin Castilla" , definition: "2015 COMP SCI GRAD from USF. Was on the Dean's list in 2010"},               
 ];
